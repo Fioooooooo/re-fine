34ee3e72d86ec7efee069aaedfe4887a0e5b6788
Fio <fuyitao2018@gmail.com>
2025-06-19 17:27:24 +0800

    read_file tool 增加文件下载样式

--- components/chat/AiMessage.vue
+++ components/chat/AiMessage.vue
@@ -2,6 +2,7 @@
 import type { UIMessage } from 'ai';
 import markdownit from 'markdown-it';
 import hljs from 'highlight.js';
+import { formatBytes } from '~/utils/file.client';
 
 const md = markdownit({
   html: true,
@@ -15,7 +16,8 @@ const md = markdownit({
           hljs.highlight(str, { language: lang, ignoreIllegals: true }).value +
           '</code></pre>'
         );
-      } catch (__) {}
+      } catch (__) {
+      }
     }
 
     return '<pre><code class="hljs">' + md.utils.escapeHtml(str) + '</code></pre>';
@@ -28,10 +30,10 @@ const props = defineProps<{
 </script>
 
 <template>
-  <div class="rounded-lg max-w-full space-y-2">
+  <div class="rounded-lg max-w-full space-y-4">
     <div v-for="(part, pIdx) in message.parts?.filter((p) => ['text', 'tool-invocation'].includes(p.type))"
          :key="pIdx"
-         class="space-y-2"
+         class="space-y-4"
     >
       <div v-if="part.type === 'text'" class="max-w-none">
         <div class="markdown-content" v-html="md.render(part.text)"></div>
@@ -49,6 +51,29 @@ const props = defineProps<{
           <span class="font-bold">{{ part?.toolInvocation?.toolName }}</span>
           <span class="flex-1 overflow-hidden text-ellipsis line-clamp-1">{{ part?.toolInvocation?.args }}</span>
         </label>
+      </div>
+      <div v-if="part.type === 'tool-invocation'
+        && part.toolInvocation.toolName === 'sandbox_read_file'
+        && part.toolInvocation.state === 'result'
+        && part.toolInvocation.result?.downloadUrl"
+           class="text-sm grid grid-cols-3 gap-6 w-full"
+      >
+        <a class="block relative col-span-1 w-full overflow-hidden cursor-pointer border p-3 rounded-lg bg-elevated/50 hover:bg-elevated transition-colors duration-150"
+           :href="part.toolInvocation.result.downloadUrl"
+           target="_blank"
+        >
+          <div class="flex w-full overflow-hidden items-center">
+            <UIcon name="i-lucide-file-text" class="w-8 h-8 mr-2 rounded-lg object-cover" />
+            <div class="flex-1 min-w-0 overflow-hidden">
+              <div class="font-semibold text-sm overflow-hidden whitespace-nowrap text-ellipsis">
+                {{ part.toolInvocation.result.downloadUrl.split('/').pop() }}
+              </div>
+              <div class="text-xs text-gray-500 flex items-center w-full">
+                点击下载
+              </div>
+            </div>
+          </div>
+        </a>
       </div>
       <div v-if="part.type === 'tool-invocation'
         && part.toolInvocation.toolName === 'search_web'
